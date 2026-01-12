87ea1fbac1055fba17538ad313b2a4acbbe89598
default <marko.skorupan@stu.ocsb.ca>
2026-01-12 18:04:49 +0000

    Set up the project file, and added the basic program structure

---
+++ main.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Marko Skorupan
+ * @version 1.0.0
+ * @date 2026-01-10
+ * @fileoverview Console-based soccer quiz game.
+ */
+
+// VARIABLES
+let score: number = 0;
+let questionNumber: number = 0;
+let userAnswer: string = "";
+
